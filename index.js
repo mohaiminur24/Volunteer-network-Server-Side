a75c8e87d8f5cffa18255cc153bf028b4bab7e5c
John Doe <mohaiminur24@gmail.com>
2023-05-24 18:30:44 +0600

    vercel deploy is complete

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // connection with database is here
     const db_event = client.db('VolunteerNetwork').collection('event');
